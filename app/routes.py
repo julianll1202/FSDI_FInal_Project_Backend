54ed57c4bec851137e8a000b980390efc4cd07b6
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-01-25 19:40:38 -0800

    Added login validation

--- app/routes.py
+++ app/routes.py
@@ -14,10 +14,14 @@ def index():
 def my_name():
     return "Hi, my name is Julian"
 
+# Creates a new user through the sign up form
 @app.post("/signup")
 def save_user():
     user = request.get_json()
-
+    users = get_all_users()
+    for usr in users:
+        if user['email'] == usr['email']:
+            return abort(208, "Email already on database")
     if user is None:
         return abort(400, "User could not be created")
     u = User(first_name=str(user['first-name']), last_name=str(user['last-name']),
@@ -25,3 +29,30 @@ def save_user():
     db.session.add(u)
     db.session.commit()
     return json.dumps(user)
+
+@app.post("/login")
+def validate_user_cred():
+    user_cred = request.get_json()
+    users = get_all_users()
+    for usr in users:
+        if user_cred['email'] == usr['email']:
+            if user_cred['password'] == usr['password']:
+                return json.dumps(usr)
+            else:
+                return abort(400, "Invalid user credentials")
+
+# Gets the list of all the users
+@app.get("/user-list")
+def get_all_users():
+    users = User.query.all()
+    user_list = []
+    for u in users:
+        usr ={
+            "id": u.id,
+            "first-name": u.first_name,
+            "last-name": u.last_name,
+            "email": u.email,
+            "password": u.password
+        }
+        user_list.append(usr)
+    return user_list
