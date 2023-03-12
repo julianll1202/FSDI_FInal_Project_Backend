3d6345f55ac05c0be80249f798d1f75dbe055e5f
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-03-12 10:26:53 -0700

    to deploy

--- application.py
+++ application.py
@@ -1,5 +1,11 @@
 from app import app
 
 
-app.debug = True
-app.run(port=8000)
+#app.debug = True
+#app.run(port=8000)
+
+# activate venv
+# FLASK_APP=application.py
+# XXXX flask run --port=8000
+
+# run a coomand on linux that stays running after you leave the session
